40e777fc526fd2d050c0f4d4705602e8a8be6d0b
Andrii Syrokomskyi <signmotion@gmail.com>
2024-01-31 19:54:43 +0100

    Refactoring tests.

--- test/domain_price_test.dart
+++ test/domain_price_test.dart
@@ -3,24 +3,23 @@ import 'package:test/test.dart';
 
 void main() {
   group('endingPrice', () {
+    const name = 'mykind456932';
+
     test('price with existing ending domain name', () {
-      expect(const DomainPrice('mykind456932.academy').endingPriceEuro, 29);
-      expect(const DomainPrice('mykind456932.com').endingPriceEuro, 12);
-      expect(const DomainPrice('mykind456932.com.mx').endingPriceEuro, 19);
-      expect(const DomainPrice('mykind456932.zone').endingPriceEuro, 29);
+      expect(const DomainPrice('$name.academy').endingPriceEuro, 29);
+      expect(const DomainPrice('$name.com').endingPriceEuro, 12);
+      expect(const DomainPrice('$name.com.mx').endingPriceEuro, 19);
+      expect(const DomainPrice('$name.zone').endingPriceEuro, 29);
       expect(
-        const DomainPrice('mykind456932.com.zone').endingPriceEuro,
-        const DomainPrice('mykind456932.zone').endingPriceEuro,
+        const DomainPrice('$name.com.zone').endingPriceEuro,
+        const DomainPrice('$name.zone').endingPriceEuro,
       );
     });
 
     test('price with non existing ending domain name', () {
-      expect(const DomainPrice('mykind456932.academy123').endingPriceEuro, -1);
-      expect(const DomainPrice('mykind456932.xxxcomnone').endingPriceEuro, -1);
-      expect(
-        const DomainPrice('mykind456932.mypersonalzone').endingPriceEuro,
-        -1,
-      );
+      expect(const DomainPrice('$name.academy123').endingPriceEuro, -1);
+      expect(const DomainPrice('$name.xxxcomnone').endingPriceEuro, -1);
+      expect(const DomainPrice('$name.mypersonalzone').endingPriceEuro, -1);
     });
   });
 }
